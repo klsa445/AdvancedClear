93191599208bb2c43879ad3d0470a373aa00515c
klsa445 <BJ3158941721@icloud.com>
2024-10-04 11:25:22 +0800

    Upload to github

---
+++ src/main/java/net/liziyu352/advancedclear/AdvancedClear.java
@@ -0,0 +1,17 @@
+package net.liziyu352.advancedclear;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedClear extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
